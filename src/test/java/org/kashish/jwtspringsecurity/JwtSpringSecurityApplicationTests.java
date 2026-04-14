b892a22b6e1a32cfde0a6eab71a9359c5ed1d952
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-14 16:49:57 +0530

    Initial commit - JWT Spring Security setup

---
+++ src/test/java/org/kashish/jwtspringsecurity/JwtSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kashish.jwtspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
