252203087b92d8e2c98c7b0bdbc9eb6a41147446
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-18 11:30:12 +0530

    enable spring customize security

---
+++ src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
@@ -0,0 +1,10 @@
+package org.kashish.jwtspringsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class Springconfig {
+
+}
