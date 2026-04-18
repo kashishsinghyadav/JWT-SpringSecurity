dfeae83e9f7bc50ec2bb1f8709605da43b3a1b74
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-18 11:34:10 +0530

    add security filterchain

--- src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
+++ src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
@@ -1,10 +1,19 @@
 package org.kashish.jwtspringsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Springconfig {
 
+
+    @Bean
+    // configuration of enabling suctomize filer chain
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        return httpSecurity.build(); // return the object of secuirty filer chain
+    }
 }
