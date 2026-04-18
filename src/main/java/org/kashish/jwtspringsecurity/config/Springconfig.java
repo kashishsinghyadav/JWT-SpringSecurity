2d56a864826753717e10dc51c3dabeac93b5c535
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-18 11:44:06 +0530

    configure the formlogin

--- src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
+++ src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
@@ -2,6 +2,7 @@ package org.kashish.jwtspringsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,14 @@ public class Springconfig {
 
     @Bean
     // configuration of enabling suctomize filer chain
+    //1.disable csrf token
+    //2. make sure all request authticated
+    //3. enable form login so the form is visible to acess page
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.csrf(customizer -> customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+
         return httpSecurity.build(); // return the object of secuirty filer chain
     }
 }
