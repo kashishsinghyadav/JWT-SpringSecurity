418e40c4e52afa6b3820268474eff380256c90ab
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-18 11:55:28 +0530

    make session as stateless

--- src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
+++ src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,10 +18,12 @@ public class Springconfig {
     //1.disable csrf token
     //2. make sure all request authticated
     //3. enable form login so the form is visible to acess page
+    //4. enabling for post man or rest client
+    //5. mark session as stateless
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-        httpSecurity.csrf(customizer -> customizer.disable());
-        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.csrf(customizer -> customizer.disable()).authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .httpBasic((Customizer.withDefaults())).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build(); // return the object of secuirty filer chain
     }
