4738ba76660ad3793c8be0b23fde1a931e5a873e
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-20 21:45:20 +0530

    add inmemory userdetail interface

--- src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
+++ src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,7 +29,17 @@ public class Springconfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request -> request.anyRequest().authenticated())
         .formLogin(Customizer.withDefaults())
         .httpBasic((Customizer.withDefaults())).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
         return httpSecurity.build(); // return the object of secuirty filer chain
     }
+
+    @Bean
+    // userdetail serivec is interface
+    public UserDetailsService userDetailsService(){
+        // userdetails also interface
+        UserDetails user1 = User.withDefaultPasswordEncoder().username("kashish").password("<PASSWORD>").roles("USER").build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder().username("admin").password("<PASSWORD>").roles("ADMIN").build();
+        return  new InMemoryUserDetailsManager(user1,user2);
+    }
+
 }
