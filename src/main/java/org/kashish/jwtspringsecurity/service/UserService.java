68783828661cbe63a24059fd57ebe8835beaaa03
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-22 21:54:54 +0530

    use password bcryptencoder

--- src/main/java/org/kashish/jwtspringsecurity/service/UserService.java
+++ src/main/java/org/kashish/jwtspringsecurity/service/UserService.java
@@ -4,6 +4,7 @@ package org.kashish.jwtspringsecurity.service;
 import org.kashish.jwtspringsecurity.model.Users;
 import org.kashish.jwtspringsecurity.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +13,9 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
     public Users register(Users user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
