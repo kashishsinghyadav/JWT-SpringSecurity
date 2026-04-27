0cfe89bf75815dc5b659386d02b2fc408cd3d404
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-04-27 21:08:23 +0530

    add authmanager

--- src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
+++ src/main/java/org/kashish/jwtspringsecurity/config/Springconfig.java
@@ -3,13 +3,16 @@ package org.kashish.jwtspringsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -57,6 +60,11 @@ public class Springconfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 
         return provider;
+    }
+    // get hold on auth manager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
